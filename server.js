fcaa73e8d90f1cf45e3b1845a62b1c8d55735b1f
IndraRusyana <indrarusyana@gmail.com>
2023-11-02 19:12:47 +0000

    update from google cloud

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 8000;
 const HOST = '0.0.0.0'
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, HOST)
-console.log(`Running on http://${HOST}:${PORT}`)
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
